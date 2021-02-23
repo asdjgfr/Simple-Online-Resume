a0b167d069f988cc4be6d3c31478f38365c13c05
liu <nian.liu@keepsoft.net>
2021-02-23 13:20:03 +0800

    修复md文件中图片路径的问题

--- bindata/asset.go
+++ bindata/asset.go
@@ -6,23 +6,23 @@ import (
 	"github.com/jessevdk/go-assets"
 )
 
-var _Assets3737a75b5254ed1f6d588b40a3449721f9ea86c2 = "<!DOCTYPE html>\r\n<html lang=\"zh-CN\">\r\n<head>\r\n    <meta charset=\"UTF-8\">\r\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n    <meta name=\"apple-mobile-web-app-capable\" content=\"yes\"/>\r\n    <link rel=\"shortcut icon\" href=\"/assets/favicon.ico\">\r\n    <title>{{.title}}</title>\r\n    <link rel=\"stylesheet\" href=\"/assets/style.css\">\r\n</head>\r\n<body>\r\n<header>\r\n    <div class=\"header\" id=\"header\">\r\n        <strong>{{.i18n.Header}}</strong>\r\n    </div>\r\n</header>\r\n<div id=\"resume\">{{.resume}}</div>\r\n<div class=\"tool-bar\" id=\"tool-bar\">\r\n    <button id=\"download\">{{.i18n.Download}}</button>\r\n    <button id=\"send-to-email\">{{.i18n.SendToEmail}}</button>\r\n</div>\r\n<script src=\"/assets/resume.js\"></script>\r\n</body>\r\n</html>"
+var _Assets3737a75b5254ed1f6d588b40a3449721f9ea86c2 = "<!DOCTYPE html>\n<html lang=\"zh-CN\">\n<head>\n    <meta charset=\"UTF-8\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n    <meta name=\"apple-mobile-web-app-capable\" content=\"yes\"/>\n    <link rel=\"shortcut icon\" href=\"/assets/favicon.ico\">\n    <title>{{.title}}</title>\n    <link rel=\"stylesheet\" href=\"/assets/style.css\">\n</head>\n<body>\n<header>\n    <div class=\"header\" id=\"header\">\n        <strong>{{.i18n.Header}}</strong>\n    </div>\n</header>\n<div id=\"resume\">{{.resume}}</div>\n<div class=\"tool-bar\" id=\"tool-bar\">\n    <button id=\"download\">{{.i18n.Download}}</button>\n    <button id=\"send-to-email\">{{.i18n.SendToEmail}}</button>\n</div>\n<script src=\"/assets/resume.js\"></script>\n</body>\n</html>"
 
 // Assets returns go-assets FileSystem
 var Assets = assets.NewFileSystem(map[string][]string{"/": []string{"html"}, "/html": []string{"index.tmpl"}}, map[string]*assets.File{
-	"/html": &assets.File{
+	"/": &assets.File{
+		Path:     "/",
+		FileMode: 0x800001ff,
+		Mtime:    time.Unix(1614048724, 1614048724611105700),
+		Data:     nil,
+	}, "/html": &assets.File{
 		Path:     "/html",
 		FileMode: 0x800001ff,
-		Mtime:    time.Unix(1603076066, 1603076066063414900),
+		Mtime:    time.Unix(1603077881, 1603077881000000000),
 		Data:     nil,
 	}, "/html/index.tmpl": &assets.File{
 		Path:     "/html/index.tmpl",
 		FileMode: 0x1ff,
-		Mtime:    time.Unix(1603076066, 1603076066063414900),
+		Mtime:    time.Unix(1603077881, 1603077881000000000),
 		Data:     []byte(_Assets3737a75b5254ed1f6d588b40a3449721f9ea86c2),
-	}, "/": &assets.File{
-		Path:     "/",
-		FileMode: 0x800001ff,
-		Mtime:    time.Unix(1603076626, 1603076626328369000),
-		Data:     nil,
 	}}, "")
--- fileWatcher/fileWatcher.go
+++ fileWatcher/fileWatcher.go
@@ -1,12 +1,16 @@
 package fileWatcher
 
 import (
+	"bufio"
+	"bytes"
 	"github.com/bep/debounce"
 	"github.com/fsnotify/fsnotify"
+	"io"
 	"log"
 	"myModule/types"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
 )
 
@@ -56,13 +60,46 @@ func WatchResume(cb Cb, conf types.Config) {
 }
 
 func rebuildResume(conf types.Config) {
+	//替换路径
+	var resumePath="./assets/resume.md"
+	err:= replacePath(resumePath)
+	if err!=nil {
+		log.Fatalf("替换路径失败： %s\n", err)
+	}
 	//重新生成html和pdf
-	cmd := exec.Command("bash", "-c", "./bin/pandoc ./assets/resume.md -t html -o ./assets/resume.tmpl ; ./bin/pandoc --highlight-style zenburn ./assets/resume.md --pdf-engine=xelatex -t latex -V CJKmainfont='"+conf.Font+"' -V colorlinks -V urlcolor=NavyBlue -o ./assets/resume.pdf")
+	cmd := exec.Command("bash", "-c", "./bin/pandoc "+resumePath+".tmp.md -t html -o ./assets/resume.tmpl ; ./bin/pandoc --highlight-style zenburn "+resumePath+".tmp.md --pdf-engine=xelatex -t latex -V CJKmainfont='"+conf.Font+"' -V colorlinks -V urlcolor=NavyBlue -o ./assets/resume.pdf")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		log.Fatalf("生成resume失败： %s\n", err)
 	}
 	log.Println("生成resume成功")
+	_ = os.Remove(resumePath+".tmp.md")
+}
+
+func replacePath(path string) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	buf := bufio.NewReader(f)
+	var result bytes.Buffer
+	for {
+		a, _, c := buf.ReadLine()
+		if c == io.EOF {
+			break
+		}
+		result.WriteString(strings.Replace(string(a), "./", "./assets/", -1 ))
+		result.WriteString("\n")
+	}
+
+	fw, err := os.OpenFile(path+".tmp.md", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	w := bufio.NewWriter(fw)
+	_, err =w.WriteString(result.String())
+	if err != nil {
+		return err
+	}
+	err=w.Flush()
+	return err
 }
